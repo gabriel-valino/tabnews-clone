15adeb31ea513b6ae2b36ccd0c829c3b355030fb
Gabriel <gabrielvalino25@hotmail.com>
2025-10-31 23:18:58 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -3,13 +3,16 @@ import database from "infra/database.js";
 beforeAll(cleanDatabase);
 
 async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; create schema public;");
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 }
 
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("DELETE to /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running a not allowed method", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+      expect(response.status).toBe(405);
+    });
   });
-
-  expect(response.status).toBe(405);
 });
