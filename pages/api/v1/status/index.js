67b572677080d504c4b7eb8eebf43ef65014bda6
Gabriel <gabrielvalino25@hotmail.com>
2026-04-06 21:50:48 +0000

    refactor: inline `createRouter()` chain in all API route exports

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,12 +3,10 @@ import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import authorization from "models/authorization";
 
-const router = createRouter();
-
-router.use(controller.injectAnonymousOrUser);
-router.get(getHandler);
-
-export default router.handler(controller.errorHandlers);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .get(getHandler)
+  .handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const userTryingToPost = request.context.user;
